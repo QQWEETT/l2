c70d6bd051b914ba2412404af2b63c028aeeac9b
QQWEETT <94145619+QQWEETT@users.noreply.github.com>
2022-05-13 13:40:16 +0300

    Update main.go

--- basic/q8/main.go
+++ basic/q8/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
-	"io"
 	"os"
-	"os/exec"
 	"strconv"
 	"strings"
 )
+
+type process struct {
+	name string
+	*os.Process
+}
+
+type cmd struct {
+	name string
+	args []string
+}
 
 const (
 	CommandEcho = "echo"
@@ -17,162 +26,181 @@ const (
 	CommandPwd  = "pwd"
 	CommandExit = "quit"
 	CommandPs   = "ps"
-	ExitText    = "Exit"
 )
 
-type Command interface {
-	Exec(args ...string) ([]byte, error)
-}
-
-// echoCmd выполняет unix-команду echo и возвращает результат в байтах
-type echoCmd struct {
-}
-
-func (e *echoCmd) Exec(args ...string) ([]byte, error) {
-	return exec.Command("echo", args...).Output()
-}
-
-// cdCmd - изменяет директорию
-type cdCmd struct {
-}
-
-func (c *cdCmd) Exec(args ...string) ([]byte, error) {
-	dir := args[0]
-	err := os.Chdir(dir)
-	if err != nil {
-		return nil, err
-	}
-	dir, err = os.Getwd()
-	if err != nil {
-		return nil, err
-	}
-
-	return []byte(dir), nil
-}
-
-// pwdCmd - выводит путь директории в которой находится терминал
-type pwdCmd struct {
-}
-
-func (p *pwdCmd) Exec(args ...string) ([]byte, error) {
-	dir, err := os.Getwd()
-	return []byte(dir), err
-}
-
-// killCmd - убивает запущенный процесс
-type killCmd struct {
-}
-
-func (k *killCmd) Exec(args ...string) ([]byte, error) {
-	pid, err := strconv.Atoi(args[0])
-	if err != nil {
-		fmt.Println(err.Error())
-		return nil, err
-	}
-	process, err := os.FindProcess(pid)
-	if err != nil {
-		fmt.Println(err.Error())
-		return nil, err
-	}
-
-	err = process.Kill()
-	if err != nil {
-		fmt.Println(err.Error())
-		return nil, err
-	}
-	return []byte("killed"), nil
-}
-
-// psCmd - Выводит работающие процессы
-type psCmd struct {
-}
-
-func (p *psCmd) Exec(args ...string) ([]byte, error) {
-	return exec.Command("ps").Output()
-}
-
-// Shell - UNIX-шелл-утилита с поддержкой ряда простейших команд
-type Shell struct {
-	command Command
-	output  io.Writer
-}
-
-func (s *Shell) SetCommand(cmd Command) {
-	s.command = cmd
-}
-
-// run - выполнение конкретной команды
-func (s *Shell) run(args ...string) {
-	b, err := s.command.Exec(args...)
-	_, err = fmt.Fprintln(s.output, string(b))
-	if err != nil {
-		fmt.Println("[err]", err.Error())
-		return
+func (c cmd) run() (string, error) {
+	switch c.name {
+	default:
+		return "", errors.New(c.name + ": command not found")
+	case CommandExit:
+		os.Exit(0)
+		return "", nil
+	case CommandCd:
+		return "", cd(c.args)
+	case CommandPwd:
+		return os.Getwd()
+	case CommandEcho:
+		return echo(c.args), nil
+	case CommandKill:
+		return "", kill(c.args)
+	case CommandPs:
+		return getPs(), nil
 	}
 }
 
-// ExecuteCommands Исполняет команды, которые ввел пользователь
-func (s *Shell) ExecuteCommands(cmds []string) {
-	for _, command := range cmds {
-		args := strings.Split(command, " ")
-
-		com := args[0]
-		if len(args) > 1 {
-			args = args[1:]
+func cd(args []string) (err error) {
+	var path string
+	switch {
+	case len(args) == 0:
+		path, err = os.UserHomeDir()
+		if err != nil {
+			return err
 		}
+	case strings.HasPrefix(args[0], "~"):
+		path, err = os.UserHomeDir()
+		if err != nil {
+			return err
+		}
+		path = path + args[0][len("~"):]
+	default:
+		path = args[0]
+	}
 
-		switch com {
-		case CommandEcho:
-			cmd := &echoCmd{}
-			s.SetCommand(cmd)
+	return os.Chdir(path)
+}
 
-		case CommandCd:
-			cmd := &cdCmd{}
-			s.SetCommand(cmd)
+func shellPath() (string, error) {
+	path, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	if strings.HasPrefix(path, home) {
+		path = "~" + path[len(home):]
+	}
+	return path, nil
+}
 
-		case CommandKill:
-			cmd := &killCmd{}
-			s.SetCommand(cmd)
+func echo(args []string) string {
+	return strings.Join(args, " ")
+}
 
-		case CommandPwd:
-			cmd := &pwdCmd{}
-			s.SetCommand(cmd)
+func kill(args []string) error {
+	if len(args) == 0 {
+		return errors.New("pid is not specified")
+	}
+	ps := make([]process, len(args))
+	for i, arg := range args {
+		pid, err := strconv.Atoi(arg)
+		if err != nil {
+			return err
+		}
+		p, ok := pss[pid]
+		if !ok {
+			return errors.New("pid " + args[i] + " not found")
+		}
+		ps[i] = p
+	}
 
-		case CommandPs:
-			cmd := &psCmd{}
-			s.SetCommand(cmd)
+	for _, p := range ps {
+		err := p.Kill()
+		if err != nil {
+			return err
+		}
+	}
 
-		case CommandExit: // завершение программы
-			_, err := fmt.Fprintln(s.output, ExitText)
-			if err != nil {
-				fmt.Println("[err]", err.Error())
-				return
-			}
-			os.Exit(1)
+	return nil
+}
+
+func getPs() string {
+	psString := "PID\tCMD\n"
+	for id, p := range pss {
+		psString += fmt.Sprintf("%d\t%s", id, p.name)
+	}
+	return psString
+}
+
+func execPipeline(ps []cmd) (out string, err error) {
+	for _, p := range ps {
+		if out != "" {
+			p.args = append(p.args, out)
+		}
+		out, err = p.run()
+		if err != nil {
+			return "", err
+		}
+	}
+	return out, nil
+}
+
+func parseInput(input string) (ps []cmd, err error) {
+	pipes := strings.FieldsFunc(input, func(r rune) bool { return r == '|' })
+	if len(pipes) == 0 {
+		return nil, errors.New("syntax error near unexpected token '|'")
+	}
+	for _, p := range pipes {
+		cmdString := strings.Fields(p)
+		switch len(cmdString) {
+		case 0:
+			return nil, errors.New("syntax error near unexpected token '|'")
+		case 1:
+			ps = append(ps, cmd{
+				name: cmdString[0],
+			})
 		default:
-			fmt.Println("Такой команды не существует")
-			continue
+			ps = append(ps, cmd{
+				name: cmdString[0],
+				args: cmdString[1:],
+			})
 		}
-		s.run(args...)
 	}
+	return ps, nil
+}
+
+var pss map[int]process
+
+func init() {
+	pss = make(map[int]process)
+	mPid := os.Getpid()
+	mp, err := os.FindProcess(mPid)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	pss[mPid] = process{"shell", mp}
 }
 
 func main() {
-	// Читает из стандартного ввода
-	scan := bufio.NewScanner(os.Stdin)
-
-	// устанавливается общий вывод результата команд
-	var output = os.Stdout
-
-	shell := &Shell{output: output}
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		fmt.Print("command: ")
-
-		if scan.Scan() {
-			line := scan.Text()
-			cmds := strings.Split(line, " | ")
-
-			shell.ExecuteCommands(cmds)
+		path, err := shellPath()
+		if err != nil {
+			fmt.Print("$ ")
+		} else {
+			fmt.Print(path + " $ ")
+		}
+		if scanner.Scan() {
+			input := scanner.Text()
+			pipes, err := parseInput(input)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			out, err := execPipeline(pipes)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			if out != "" {
+				fmt.Println(out)
+			}
+		} else {
+			fmt.Println()
+			break
 		}
 	}
+
 }
